d0496c6e49c2495ca41fa6d22ee61e30a433a2e7
Jeppe Lindberg <jepp3441@gmail.com>
2023-03-30 08:09:32 +0200

    prepare

--- prepare_singularity.py
+++ prepare_singularity.py
@@ -1,12 +1,13 @@
 import os
 import shutil
+from scripts import write
 
 
 def main():
     base_dir = os.path.abspath(os.path.dirname(__file__))
     move_dir = os.path.join(base_dir, "image_folder")
     all_files = os.listdir(base_dir)
-    
+
     for i, file in enumerate(all_files):
         all_files[i] = os.path.join(base_dir, file)
 
