d9fabe37a47316bd8c6f08a757a7314d57dca001
Jeppe Lindberg <jepp3441@gmail.com>
2023-03-30 08:23:09 +0200

    Prepare

--- prepare_singularity.py
+++ prepare_singularity.py
@@ -11,9 +11,8 @@ def main():
     for i, file in enumerate(all_files):
         all_files[i] = os.path.join(base_dir, file)
 
-    ignore_files = ("singularity.def", "environment.yml", "requirements.txt", "prepare_singularity.py")
+    ignore_files = ("singularity.def", "environment.yml", "requirements.txt", "prepare_singularity.py", ".git")
     move_files = [file for file in all_files if not file.endswith(ignore_files)]
-    move_files = [file for file in move_files if (os.path.sep + ".git" + os.path.sep) not in file]
 
     os.mkdir(move_dir)
     for file in move_files:
