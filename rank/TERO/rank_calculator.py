b19ae28f57c6c6efcce1c0e32bee403638ad1ce0
Jeppe Lindberg <jepp3441@gmail.com>
2023-04-14 09:49:51 +0200

    Ranking ATISE and TERO
    Add models ATISE and TERO
    
    Rank ATISE and TERO
    
    remove models
    
    Readded models

--- rank/TERO/rank_calculator.py
+++ rank/TERO/rank_calculator.py
@@ -2,6 +2,8 @@ import torch
 import numpy as np
 import time
 import datetime
+from scripts import remove_unwanted_symbols_from_str
+from dateutil.relativedelta import relativedelta
 
 class RankCalculator:
     def __init__(self, params, model):
@@ -9,11 +11,24 @@ class RankCalculator:
         self.kg = model.kg
         self.model = model
 
+        if self.dataset_name in ['icews14']:
+            self.start_sim_date = datetime.date(2014, 1, 1)
+            self.end_sim_date = datetime.date(2015, 1, 1)
+            self.delta_sim_date = datetime.timedelta(days=1)
+        elif self.dataset_name in ['wikidata12k']:
+            self.start_sim_date = datetime.date(1, 1, 1)
+            self.end_sim_date = datetime.date(2021, 1, 1)
+            self.delta_sim_date = relativedelta(years=1)
+        elif self.dataset_name in ['yago11k']:
+            self.start_sim_date = datetime.date(1, 1, 1)
+            self.end_sim_date = datetime.date(2845, 1, 1)
+            self.delta_sim_date = relativedelta(years=1)
+
     def get_rank(self, scores):  # assuming the first fact is the correct fact
         return torch.sum((scores < scores[0]).float()).item() + 1
 
     def get_ent_id(self, entity):
-        return self.kg.entity_dict[entity]
+        return self.kg.entity_dict[remove_unwanted_symbols_from_str(entity)]
 
     def get_rel_id(self, relation):
         return self.kg.relation_dict[relation]
@@ -44,8 +59,8 @@ class RankCalculator:
                 sim_facts = [[head, self.get_ent_id(answer), relation, day]] + sim_facts
             case "T":
                 sim_facts = []
-                sim_date = datetime.date(2014, 1, 1)
-                while sim_date != datetime.date(2015, 1, 1):
+                sim_date = self.start_sim_date
+                while sim_date != self.end_sim_date:
                     year = sim_date.year
                     month = sim_date.month
                     day = sim_date.day
@@ -53,7 +68,7 @@ class RankCalculator:
                     sim_timestamp = str(year) + '-' + str(month) + '-' + str(day)
                     sim_facts.append([head, tail, relation, self.get_day_from_timestamp(sim_timestamp)])
 
-                    sim_date = sim_date + datetime.timedelta(days=1)
+                    sim_date = sim_date + self.delta_sim_date
 
                 sim_facts = [[head, tail, relation, self.get_day_from_timestamp(answer)]] + sim_facts
             case _:
