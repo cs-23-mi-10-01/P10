d892a5f28ac5578740746add3e8c1a6f5db6ba12
Jones <jlindb18@student.aau.dk>
2023-05-19 12:10:43 +0200

    added timeplex

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main():
     if params.embeddings == ['all']:
         params.embeddings = ['DE_TransE', 'DE_SimplE', 'DE_DistMult', 'TERO', 'ATISE','TFLEX', 'TimePlex']
     elif params.embeddings == ['ensemble']:
-        params.embeddings = ['DE_TransE', 'DE_SimplE', 'DE_DistMult', 'TERO', 'ATISE']
+        params.embeddings = ['DE_TransE', 'DE_SimplE', 'DE_DistMult', 'TERO', 'ATISE',  'TimePlex']
     if params.datasets == ['all']:
         params.datasets = ['icews14', 'wikidata12k', 'yago11k']
     if params.splits == ['all']:
