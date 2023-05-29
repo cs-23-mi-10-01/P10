d4d32d0ae96852f8102961ab615924a39a8f050d
puchasa <45094452+puchasa@users.noreply.github.com>
2023-05-29 12:50:40 +0200

    fixed timeplex

--- rank/TimePlex/rank_calculator.py
+++ rank/TimePlex/rank_calculator.py
@@ -312,9 +312,9 @@ class RankCalculator:
     
     def best_prediction(self, fact_scores, range):
         fact_scores = {k: v for k, v in fact_scores.items() if k[3] not in ('-', '####-##-##')} # filter
-        fact_scores = sorted(fact_scores.items(), key= lambda item: item[1]) # sort
-        result = [] # this part is for range
-        for i, j in enumerate(fact_scores):
-            if i+1 >= range[0] and i+1 <= range[1]:
-                result.append(j[0][3])
+        fact_scores = sorted(fact_scores.items(), key= lambda item: item[1], reverse=True) # sort
+        #result = [] # this part is for range
+        #for i, j in enumerate(fact_scores):
+        #    if i+1 >= range[0] and i+1 <= range[1]:
+        #        result.append(j[0][3])
         return fact_scores[0][0][3]
