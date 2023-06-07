f14b8e9b677ae52aeba1d27b12165b242b4472ef
Jones <jlindb18@student.aau.dk>
2023-06-07 10:08:56 +0200

    one third update

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ def main():
     parser = argparse.ArgumentParser()
 
     #python -task rank -dataset icews14 -embedding DE_TransE -split all
-    parser.add_argument('-task', type=str, default='ablation_one_forth_property', choices=['statistics', 'rank', 'formatlatex', 'split_dataset', 'generate_quads', 'best_predictions', 'ensemble_naive_voting', "ensemble_decision_tree", "ablation_overall", "ablation_property", "ablation_false_property", "ablation_time_density", "ablation_target", "ablation_no_property", "ablation_one_forth_property", "ablation_only_property", "ablation_only_target", "ablation_only_overall", "ablation_only_time_density"])
-    parser.add_argument('-dataset', type=str, default='icews14', choices=['all', 'icews14', 'wikidata11k', 'wikidata12k', 'yago11k'])
+    parser.add_argument('-task', type=str, default='statistics', choices=['statistics', 'rank', 'formatlatex', 'split_dataset', 'generate_quads', 'best_predictions', 'ensemble_naive_voting', "ensemble_decision_tree", "ablation_overall", "ablation_property", "ablation_false_property", "ablation_time_density", "ablation_target", "ablation_no_property", "ablation_one_forth_property", "ablation_only_property", "ablation_only_target", "ablation_only_overall", "ablation_only_time_density"])
+    parser.add_argument('-dataset', type=str, default='all', choices=['all', 'icews14', 'wikidata11k', 'wikidata12k', 'yago11k'])
     parser.add_argument('-split', type=str, default='original', choices=['all', 'original', '1', '2', '3'])
-    parser.add_argument('-embedding', type=str, default='ensemble', choices=['all','ensemble', 'DE_TransE', 'DE_SimplE', 'DE_DistMult', 'TERO', 'ATISE', 'TFLEX','TimePlex', 'overall_scores'])
+    parser.add_argument('-embedding', type=str, default='overall_scores', choices=['all','ensemble', 'DE_TransE', 'DE_SimplE', 'DE_DistMult', 'TERO', 'ATISE', 'TFLEX','TimePlex', 'overall_scores'])
 
     args = parser.parse_args()
     params = Parameters(args)
